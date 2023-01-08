75471ef7cdbeb0240046c409817d459f58cb6fcc
pavanbhat19999 <prbhat07@gmail.com>
2023-01-08 15:43:05 +0530

    create note successful

--- force-app/main/default/lwc/notesInsert/notesInsert.js
+++ force-app/main/default/lwc/notesInsert/notesInsert.js
@@ -1,3 +1,31 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api,wire } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
+import NOTES_OBJ from '@salesforce/schema/MyNotes__c';
+import Description from '@salesforce/schema/MyNotes__c.Description__c';
 
-export default class NotesInsert extends LightningElement {}
+export default class NotesInsert extends LightningElement {
+
+note;
+
+handleOnChange(event){
+    console.log('changes ');
+    console.log(event.target.value);
+    this.note = event.target.value;
+}
+handleclick(){
+    console.log('Button clicked');
+    const fields = {};
+    fields[Description.fieldApiName] = this.note;
+
+    const recordInput = { apiName: NOTES_OBJ.objectApiName, fields };
+
+    createRecord(recordInput)
+        .then(account => {
+            this.accountId = account.id;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+}
